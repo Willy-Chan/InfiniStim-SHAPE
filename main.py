942f47794d6b17ee1a453157a7510242a4dd4fb6
Willy-Chan <106504264+Willy-Chan@users.noreply.github.com>
2023-07-24 12:25:44 -0700

    new variant that creates quadrant images and also colorizes them to exact specifications. saves all images in a standardized format of component numbers followed by the color

--- main.py
+++ main.py
@@ -44,16 +44,62 @@ def combine_images(folder_path, output_path):
 
     full_image.save(output_path)
     print(f"Full image created and saved as {output_path}")
+    return output_path
+
 
 
 if __name__ == "__main__":
     # Set the folder path where the quadrant images are located
-    folder_path = './component_folder'
+    folder_path = 'REDACTED'
 
 
     # Set the output path for the full image
-    output_path = './result'
+    output_path = 'REDACTED'
 
     # Generate stimuli
-    for i in range(25):
-        combine_images(folder_path, output_path)
+    # Generate stimuli
+    for i in range(20):
+        output = combine_images(folder_path, output_path)
+        picture = Image.open(output)
+
+
+        # Get the size of the image
+        width, height = picture.size
+
+        # RED IMAGES:
+        for x in range(width):
+            for y in range(height):
+                current_color = picture.getpixel((x, y))
+                if current_color[3] != 0:  # Check if the pixel is not fully transparent
+                    new_color = (224, 76, 76, current_color[3])  # Keep the original alpha value
+                    picture.putpixel((x, y), new_color)
+
+        red_output_path = os.path.splitext(output)[0] + '_red.png'
+        picture.save(red_output_path)
+
+        picture = Image.open(output)  # Reset to original image
+
+        # GREEN IMAGES:
+        for x in range(width):
+            for y in range(height):
+                current_color = picture.getpixel((x, y))
+                if current_color[3] != 0:  # Check if the pixel is not fully transparent
+                    new_color = (94, 224, 76, current_color[3])  # Keep the original alpha value
+                    picture.putpixel((x, y), new_color)
+
+        green_output_path = os.path.splitext(output)[0] + '_green.png'
+        picture.save(green_output_path)
+
+        picture = Image.open(output)  # Reset to original image
+
+        # WHITE IMAGES:
+        for x in range(width):
+            for y in range(height):
+                current_color = picture.getpixel((x, y))
+                if current_color[3] != 0:  # Check if the pixel is not fully transparent
+                    new_color = (255, 255, 255, current_color[3])  # Keep the original alpha value
+                    picture.putpixel((x, y), new_color)
+
+        white_output_path = os.path.splitext(output)[0] + '_white.png'
+        picture.save(white_output_path)
+
